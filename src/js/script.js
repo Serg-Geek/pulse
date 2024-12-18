082549ea947786714f70976f3471337dda36553d
Serg-Geek <svgordienko@yandex.ru>
2024-12-18 10:55:38 +0400

    added adaptation added animation

--- src/js/script.js
+++ src/js/script.js
@@ -61,5 +61,46 @@ $(document).ready(function () {
             $('.overlay, #order').fadeIn('slow');
         });
     });
-    // $('.feed-form').validate
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    // $('.form').validate();
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
+
+    // scroll-smooth and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
 });
